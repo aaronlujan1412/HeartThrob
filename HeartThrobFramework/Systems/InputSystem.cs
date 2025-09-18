b99d8e2804ea0a9ffdde129cf4cf4554de0cc13d
aaronlujan1412 <aaron.lujan1412@gmail.com>
2025-09-17 19:29:32 -0600

    feat: clea input system to single line character inputs

--- HeartThrobFramework/Systems/InputSystem.cs
+++ HeartThrobFramework/Systems/InputSystem.cs
@@ -2,57 +2,75 @@ using HeartThrobFramework.Core;
 using HeartThrobFramework.Components;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Numerics;
+using HeartThrobFramework.GameData.StateEnums;
 
 namespace HeartThrobFramework.Systems;
 
 public class InputSystem : ISystem
 {
-    public required World World { get; set; }
+    public World World { get; set; }
+    private KeyboardState _lastKeyboardState;
 
     private const float moveSpeed = 100f;
     public void Update(float deltaTime)
     {
-        var entities = World.Query<PlayerControlledComponent, VelocityComponent>();
         var keyboardState = Keyboard.GetState();
-        
+
+        var entities = World.Query<PlayerControlledComponent, VelocityComponent>();
+
         foreach (var entity in entities)
         {
             var velocity = World.GetComponent<VelocityComponent>(entity);
 
-            velocity.Value.X = 0;
-            velocity.Value.Y = 0;
-            
+            velocity.Value = Vector2.Zero;
+
+            if (keyboardState.IsKeyDown(Keys.W)) velocity.Value.Y -= moveSpeed;
             if (keyboardState.IsKeyDown(Keys.A)) velocity.Value.X -= moveSpeed;
+            if (keyboardState.IsKeyDown(Keys.S)) velocity.Value.Y += moveSpeed;
+            if (keyboardState.IsKeyDown(Keys.D)) velocity.Value.X += moveSpeed;
 
-            if (keyboardState.IsKeyDown(Keys.A))
-            {
-                velocity.Value.X = -100f;
-            }
+            //if (keyboardState.IsKeyDown(Keys.A))
+            //{
+            //    velocity.Value.X = -100f;
+            //}
 
-            if (keyboardState.IsKeyDown(Keys.D))
-            {
-                velocity.Value.X = 100f;
-            }
+            //if (keyboardState.IsKeyDown(Keys.D))
+            //{
+            //    velocity.Value.X = 100f;
+            //}
 
-            if (keyboardState.IsKeyDown(Keys.W))
-            {
-                velocity.Value.Y = -100f;
-            }
+            //if (keyboardState.IsKeyDown(Keys.W))
+            //{
+            //    velocity.Value.Y = -100f;
+            //}
 
-            if (keyboardState.IsKeyDown(Keys.S))
-            {
-                velocity.Value.Y = 100f;
-            }
+            //if (keyboardState.IsKeyDown(Keys.S))
+            //{
+            //    velocity.Value.Y = 100f;
+            //}
 
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-                Keyboard.GetState().IsKeyDown(Keys.Escape))
-            
+
+
+
+            //if (keyboardState.IsKeyDown(Keys.Escape) && 
+            //    _lastKeyboardState.IsKeyUp(Keys.Escape))
+            //{
+            //    OnMenuButtonPressed?.Invoke("esc");
+            //}
+
             World.UpdateComponent(entity, velocity);
         }
+
+        if (keyboardState.IsKeyDown(Keys.Escape) && _lastKeyboardState.IsKeyUp(Keys.Escape))
+        {
+            int commandEntity = World.CreateEntity();
+            World.AddComponent<StateToggleComponent>(commandEntity, new StateToggleComponent(GameStates.TimeStopped));
+        }
+
+        _lastKeyboardState = keyboardState;
     }
 
-    public void Render(World world, SpriteBatch spriteBatch)
-    {
-        return;
-    }
+    public void Render(SpriteBatch spriteBatch) { }
 }
+
