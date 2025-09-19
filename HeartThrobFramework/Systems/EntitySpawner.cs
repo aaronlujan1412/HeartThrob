825016a80b576233ad8df4db0fc1fc569cdd9747
aaronlujan1412 <aaron.lujan1412@gmail.com>
2025-09-19 09:54:45 -0600

    feat: create spawning system

---
+++ HeartThrobFramework/Systems/EntitySpawner.cs
@@ -0,0 +1,19 @@
+﻿using HeartThrobFramework.Core;
+using HeartThrobFramework.Factories;
+using HeartThrobFramework.GameData.Template;
+using HeartThrobFramework.Utils;
+
+namespace HeartThrobFramework.Systems
+{
+    public class EntitySpawner(EntityFactory _entityFactory)
+    {
+        public int SpawnMainCharacter(World World)
+        {
+            EntityTemplate mainCharacterTemplate = World.GetTemplate(EntityTemplateNames.Slime);
+
+            int mainCharacter = _entityFactory.Create(mainCharacterTemplate);
+
+            return mainCharacter;
+        }
+    }
+}
