c5ce5ae6d6ca8e6a4ec6b8435258248a7e406b9c
aaronlujan1412 <aaron.lujan1412@gmail.com>
2025-09-19 14:58:25 -0600

    chore: continued clean up after separating concerns from world

---
+++ HeartThrobFramework/Utils/EcsUtils.cs
@@ -0,0 +1,23 @@
+﻿using HeartThrobFramework.Components;
+using System.Reflection;
+
+namespace HeartThrobFramework.Utils
+{
+    public static class EcsUtils
+    {
+        public static IEnumerable<Type> GetAllComponentTypes(Assembly assembly)
+        {
+            var componentInterface = typeof(IComponent);
+
+            try
+            {
+                return assembly.GetTypes()
+                    .Where(t => t.IsValueType && componentInterface.IsAssignableFrom(t));
+            }
+            catch (ReflectionTypeLoadException)
+            {
+                return Enumerable.Empty<Type>();
+            }
+        }
+    }
+}
--- HeartThrobMonoGameTests/Game1.cs
+++ HeartThrobMonoGameTests/Game1.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using HeartThrobFramework.Core;
 using HeartThrobFramework.Components;
 using HeartThrobFramework.Systems;
 using HeartThrobFramework.GameData.StateEnums;
 using HeartThrobFramework.Factories;
 using HeartThrobFramework.Managers;
+using HeartThrobFramework.Core.World;
+using HeartThrobFramework.Core.ECS;
+using HeartThrobFramework.Utils;
 
 namespace HeartThrobMonoGameTests;
 
@@ -14,7 +16,8 @@ public class Game1 : Game
     private readonly World _world;
     private readonly GraphicsDeviceManager _graphics;
     private readonly InputManager _inputManager;
-    private readonly EntityFactory _entityFactory;
+    private TemplateManager _templateManager;
+    private EntityFactory _entityFactory;
     private SpriteBatch _spriteBatch;
     private EntitySpawner _spawner;
 
@@ -26,7 +29,6 @@ public class Game1 : Game
         
         _world = new World();
         _inputManager = new InputManager();
-        _entityFactory = new EntityFactory(_world, Content);
     }
     
     protected override void Initialize()
@@ -45,6 +47,21 @@ public class Game1 : Game
         _world.RegisterComponent<StateToggleComponent>();
 
         _world.AddComponent<GameStateComponent>(_world.GameStateEntity, new GameStateComponent(GameStates.TimeAdvancing));
+        
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _templateManager = new TemplateManager();
+        _templateManager.LoadAllTemplates(Content);
+
+        _entityFactory = new EntityFactory(_world, Content, _templateManager);
+
+        var renderSystem = new RenderSystem(_spriteBatch);
+        _world.RegisterSystem(renderSystem);
 
         var inputSystem = new InputSystem(_inputManager);
         _world.RegisterSystem(inputSystem);
@@ -54,20 +71,9 @@ public class Game1 : Game
 
         var gameStateSystem = new GameStateSystem(_entityFactory);
         _world.RegisterSystem(gameStateSystem);
-        
-        base.Initialize();
-    }
-
-    protected override void LoadContent()
-    {
-        _world.LoadTemplates(Content);
-
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-        var renderSystem = new RenderSystem(_spriteBatch);
-        _world.RegisterSystem(renderSystem);
 
         _spawner = new EntitySpawner(_entityFactory);
-        _spawner.SpawnMainCharacter(_world);
+        _spawner.SpawnMainCharacter();
     }
 
     protected override void Update(GameTime gameTime)
